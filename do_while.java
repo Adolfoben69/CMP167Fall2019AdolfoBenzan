ac79578c81de65894ef1032c82716fd99615a3ce
Adolfoben69 <54601139+Adolfoben69@users.noreply.github.com>
2019-10-03 15:58:47 -0400

    Add files via upload

---
+++ do_while.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @author Adolfo J. Benzan
+ *
+ */
+
+
+import java.util.Scanner;
+	public class do_while {
+		public static void Main(String[] args) {
+		
+	int main()
+	{   
+	int i = 1;
+	while (i <= 5)
+	  {
+	  printf("%d\n", i);
+	   ++i;
+	    }
+	    return 0;
+			}
+		}
+	
+
+
+	
